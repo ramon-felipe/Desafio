1b7f0fdcea197db60121eeedec24d9bf6d0668a4
danielbojczuk <39830414+danielbojczuk@users.noreply.github.com>
2021-04-22 22:58:19 -0300

    Create OperacaoCDI.cs

---
+++ Teste 1/Calculadora/Model/OperacaoCDI.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Calculadora.Model
+{
+    public class OperacaoCDI
+    {
+        public int Contrato {get;set;}
+        public DateTime DataInicio {get;set;}
+        public DateTime DataFim {get;set;}
+        public double PorcentagemCDI{get;set;}
+        public double ValorInvestido {get;set;}
+        public double ValorCorrigido {get;set;}
+
+    }
+}
